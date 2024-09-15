8f732e29549bec6e4e0528dc7b2f150156f633e0
Oganes <og.marckaryan@yandex.ru>
2024-09-15 21:25:16 +0300

    2.10. Условное ветвление

---
+++ src/js/2.10 Условное ветвление.js
@@ -0,0 +1,26 @@
+export function checkToBeTruthy(){
+    if ("0") {
+        alert( 'Привет' );
+      }
+}
+
+export function whatOfficialNameJS(){
+    let input = prompt('Какое «официальное» название JavaScript?')
+
+    if(input === "ECMAScript"){
+        alert('Верно!')
+    }else{
+        alert('Не знаете? ECMAScript!')
+    }
+}
+
+export function getNumber(){
+    let input = +prompt('Введите любое число')
+    if (input > 0) {
+        alert(1)
+    } else if(input < 0) {
+        alert(-1)
+    } else {
+        alert(0)
+    }
+}
---
+++ src/js/2.10 Условное ветвление.test.js
@@ -0,0 +1,36 @@
+import {checkToBeTruthy, whatOfficialNameJS, getNumber} from './2.10 Условное ветвление.js'
+
+
+describe("2.10.Условное ветлвение", () => {
+    test('Задание 1, Выведется ли alert?', ()=> {
+        window.alert = jest.fn();
+        checkToBeTruthy()
+        expect(window.alert).toHaveBeenCalledWith('Привет');
+    }),
+
+    test('Задание 2, Используя конструкцию if..else, напишите код, который будет спрашивать: „Какое «официальное» название JavaScript?“', ()=> {
+        jest.spyOn(window, "prompt").mockImplementation(() => "ECMAScript");
+        window.alert = jest.fn();
+        whatOfficialNameJS()
+        expect(window.alert).toHaveBeenCalledWith('Верно!');
+
+        jest.spyOn(window, "prompt").mockImplementation(() => "Dodo");
+        whatOfficialNameJS()
+        expect(window.alert).toHaveBeenCalledWith('Не знаете? ECMAScript!');
+    })
+
+    test('Задание 3,Используя конструкцию if..else, напишите код, который получает число через prompt, а затем выводит в alert: ' , ()=> {
+        jest.spyOn(window, "prompt").mockImplementation(() => 5);
+        window.alert = jest.fn();
+        getNumber()
+        expect(window.alert).toHaveBeenCalledWith(1);
+
+        jest.spyOn(window, "prompt").mockImplementation(() => -5);
+        getNumber()
+        expect(window.alert).toHaveBeenCalledWith(-1);
+
+        jest.spyOn(window, "prompt").mockImplementation(() => 0);
+        getNumber()
+        expect(window.alert).toHaveBeenCalledWith(0);
+    })
+});
