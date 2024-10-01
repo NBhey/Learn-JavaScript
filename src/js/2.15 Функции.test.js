088b95f7950fdd119f89c9ec47b2704140785ed1
Oganes <og.marckaryan@yandex.ru>
2024-10-01 20:41:41 +0300

    Функции самостоятельная работа

--- src/js/2.15 Функции.test.js
+++ src/js/2.15 Функции.test.js
@@ -53,4 +53,109 @@ describe("2.15 Функции", () => {
     expect(pow(3, 3)).toBe(27);
     expect(pow(1, 100)).toBe(1);
   });
+
+  test('Самостоятельная работа,', ()=>{
+    let thread = {
+      title: 'Поделитесь книжкой',
+      author: 'RuBrick',
+      total: 23,
+      messages: [
+        {
+          id: 13201,
+          date: '2018-01-09',
+          text: 'Посоветуйте, пожалуйста, с одной стороны подробную, а с другой доступную для понимания книгу по javascript. Спасибо',
+          author: {
+            name: 'RuBrick',
+            login: 'ru_brick',
+            reputation: 3,
+            role: 'user',
+          },
+        },
+        {
+          id: 13208,
+          date: '2018-01-12',
+          text: 'Неужели нет хорошей литературы?',
+          author: {
+            name: 'RuBrick',
+            login: 'ru_brick',
+            reputation: 3,
+            role: 'user',
+          },
+        },
+        {
+          id: 13209,
+          date: '2018-01-12',
+          text: 'в общем, NodeJS это JS + некоторые доп. модули и объекты. Тебе нужна литература по самому JS и дока на официальном сайте.',
+          author: {
+            name: 'Popov',
+            login: 'popov_ma',
+            reputation: 2310,
+            role: 'user',
+          },
+        },
+        {
+          id: 13219,
+          date: '2018-01-14',
+          text: 'В сети много сайтов с хорошими объяснениями + есть курсы.',
+          author: {
+            name: 'Void',
+            login: 'void',
+            reputation: 5005,
+            role: 'user',
+          },
+        },
+        {
+          id: 13220,
+          date: '2018-01-14',
+          text: 'Есть большая книга «JavaScript. Подробное руководство», потом смотришь документацию.',
+          author: {
+            name: 'noname',
+            login: 'noname',
+            reputation: 100,
+            role: 'user',
+          },
+        },
+        {
+          id: 13250,
+          date: '2018-01-19',
+          text: 'Или можно посмотреть видеокурсы на YouTube! А самое главное — практика! И этот форум — лучшая тренировочная площадка!',
+          author: {
+            name: 'noname',
+            login: 'noname',
+            reputation: 110,
+            role: 'user',
+          },
+        },
+        {
+          id: 13259,
+          date: '2018-01-20',
+          text: 'Понял, спасибо!',
+          author: {
+            name: 'RuBrick',
+            login: 'ru_brick',
+            reputation: 13,
+            role: 'user',
+          },
+        },
+      ],
+    };
+    
+
+    const logSpy = jest.spyOn(window.console, 'log');
+    function outputMessage(object, quantityDialog) {
+      const messageLength = object.messages.length;
+      if (messageLength === undefined)
+        console.log('Ошибка формата! В теме нет сообщений');
+    
+      for (let i = messageLength - 1; quantityDialog > 0; quantityDialog -= 1) {
+        console.log(
+          `${object.messages[i].author.name} (репутация:${object.messages[i].author.reputation}): ${object.messages[i].text}`
+        );
+        i -= 1;
+      }
+    }
+    outputMessage(thread, 3)
+    expect(logSpy).toHaveBeenCalledTimes(3)
+    expect(logSpy.mock.calls).toContainEqual(['RuBrick (репутация:13): Понял, спасибо!'],['noname (репутация:110): Или можно посмотреть видеокурсы на YouTube! А самое главное — практика! И этот форум — лучшая тренировочная площадка!'],['noname (репутация:100): Есть большая книга «JavaScript. Подробное руководство», потом смотришь документацию'])
+  })
 });
