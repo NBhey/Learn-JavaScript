90e45b976c00d24cf69e0626704db4dc1c6c5986
Oganes <og.marckaryan@yandex.ru>
2024-10-08 19:33:07 +0300

    2.17 Стрелочные функции

--- src/js/2.17 Стрелочные функции.test.js
+++ src/js/2.17 Стрелочные функции.test.js
@@ -1,8 +1,34 @@
-describe.skip('2.17 Стрелочные функции', ()=>{
-    test('Задание 1,Замените код Function Expression стрелочной функцией:', ()=>{
-        let ask = ((question, yes, no) => {
-            if (confirm(question)) yes()
-            else no();
-          })
-    })
-})
+describe("2.17 Стрелочные функции", () => {
+  test("Задание 1,Замените код Function Expression стрелочной функцией:", () => {
+    window.confirm = () => false;
+    window.alert = jest.fn();
+    let ask = (question, yes, no) => {
+      if (confirm(question)) yes();
+      else no();
+    };
+
+    ask(
+      "Вы согласны?",
+      function () {
+        alert("Вы согласились.");
+      },
+      function () {
+        alert("Вы отменили выполнение.");
+      }
+    );
+
+    expect(window.alert).toHaveBeenCalledWith("Вы отменили выполнение.");
+
+    window.confirm = () => true;
+    ask(
+      "Вы согласны?",
+      function () {
+        alert("Вы согласились.");
+      },
+      function () {
+        alert("Вы отменили выполнение.");
+      }
+    );
+    expect(window.alert).toHaveBeenCalledWith("Вы согласились.");
+  });
+});
