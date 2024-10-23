ccc396975e4354186add50cdc5b62faa55ab3c8a
Oganes <og.marckaryan@yandex.ru>
2024-10-23 20:35:12 +0300

    4.1 Объекты

--- src/js/4.1 Объекты.test.js
+++ src/js/4.1 Объекты.test.js
@@ -8,4 +8,60 @@ describe("4.1 Объекты", () => {
     delete obj.surname;
     expect(obj).toEqual({ name: "Pete" });
   });
+
+  test("Задание 2, Напишите функцию isEmpty(obj), которая возвращает true, если у объекта нет свойств, иначе false", () => {
+    const schedule = {};
+    function checkEmptyOrNotObj(obj) {
+      return Object.keys(obj) == false ? true : false;
+    }
+    expect(checkEmptyOrNotObj(schedule)).toBeTruthy();
+    schedule["name"] = "Pete";
+    expect(checkEmptyOrNotObj(schedule)).toBeFalsy();
+  });
+
+  test("Задание 3, Можно ли изменить объект, объявленный с помощью const? Как вы думаете?", () => {
+    const user = {
+      name: "John",
+    };
+    user.name = "Pete";
+    expect(user.name).toBe("Pete");
+  });
+
+  test("Задание 4, Сумма свойств объекта", () => {
+    let salaries = {
+      John: 100,
+      Ann: 160,
+      Pete: 130,
+    };
+    let sum = 0;
+    for (let key in salaries) {
+      sum += salaries[key];
+    }
+    expect(sum).toBe(390);
+    for (let key in salaries) {
+      delete salaries[key];
+    }
+    expect(!Object.keys(salaries)).toBeFalsy();
+  });
+
+  test("Задание 5, Умножаем все числовые свойства на 2", () => {
+    let menu = {
+      width: 200,
+      height: 300,
+      title: "My menu",
+    };
+    function multiplyNumeric(){
+      for (let key in menu) {
+        if (typeof menu[key] === 'number') {
+          menu[key] *= 2;
+        }
+      }
+    }
+    multiplyNumeric()
+    expect(menu).toEqual({
+      width: 400,
+      height: 600,
+      title: "My menu",
+    })
+  });
 });
