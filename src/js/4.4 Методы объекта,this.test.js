4228010b90cb8518f40f3f8d6eba3ff28f679d34
Oganes <og.marckaryan@yandex.ru>
2024-11-03 21:27:15 +0300

    4.4 Методы объекта,this

--- src/js/4.4 Методы объекта,this.test.js
+++ src/js/4.4 Методы объекта,this.test.js
@@ -1,15 +1,34 @@
-describe('4.4 Методы объекта', ()=>{
-    const logSpy = jest.spyOn(global.console, 'log')
-    test.skip('Задание 1 Использование "this" в литерале объекта',()=>{
-        function makeUser() {
-            return {
-              name: "John",
-              ref: this
-            };
-          }
-          
-          let user = makeUser();
-          
-          console.log(console.log( user.ref.name )); 
-    });
-})
+describe("4.4 Методы объекта", () => {
+  const logSpy = jest.spyOn(global.console, "log");
+  test('Задание 1 Использование "this" в литерале объекта', () => {
+    function makeUser() {
+      return {
+        name: "John",
+        ref() {
+          return this;
+        },
+      };
+    }
+    let user = makeUser();
+    console.log(user.ref().name);
+    expect(logSpy).toHaveBeenCalledWith("John");
+  });
+
+  test("Задание 2, Создайте калькулятор", () => {
+    let calculator = {
+      read() {
+        this.a = 6;
+        this.b = 4;
+      },
+      sum() {
+        return this.a + this.b;
+      },
+      mul() {
+        return this.a * this.b;
+      },
+    };
+    calculator.read()
+    expect(calculator.sum()).toBe(10);
+    expect(calculator.mul()).toBe(24);
+  });
+});
