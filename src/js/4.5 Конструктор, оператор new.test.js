78643e559f3d77cfcb3b25483e96c9f0cb134438
Oganes <og.marckaryan@yandex.ru>
2024-11-06 19:23:45 +0300

    4.5 Конструктор, оператор new, Задание 2

--- src/js/4.5 Конструктор, оператор new.test.js
+++ src/js/4.5 Конструктор, оператор new.test.js
@@ -12,9 +12,25 @@ describe('4.5 Конструктор, оператор "new"', () => {
     expect(a === b).toBeTruthy()
   });
 
-  test.skip('Задание 2, Создайте калькулятор при помоще конструктора, new Calculator', ()=>{
+  test('Задание 2, Создайте калькулятор при помоще конструктора, new Calculator', ()=>{
+    
     function Calculator(){
-        
+        this.read = function(){
+          jest.spyOn(window, "prompt").mockImplementation(()=>4)
+          this.num1 = +prompt('Введите число');
+          jest.spyOn(window, "prompt").mockImplementation(()=>4)
+          this.num2 = +prompt('Введите число');
+        },
+        this.sum = function(){
+          return this.num1 + this.num2
+        },
+        this.mul = function(){
+          return this.num1 * this.num2
+        }
     }
+    let calculator = new Calculator();
+    calculator.read();
+    expect(calculator.sum()).toBe(8)
+    expect(calculator.mul()).toBe(16)
   })
 });
