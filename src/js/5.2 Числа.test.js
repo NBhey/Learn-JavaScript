e590a2bfb247a113130ec5ef047723e7f92ea6a8
Oganes <og.marckaryan@yandex.ru>
2024-11-12 19:16:22 +0300

    5.2 Числа

--- src/js/5.2 Числа.test.js
+++ src/js/5.2 Числа.test.js
@@ -11,28 +11,45 @@ describe("5.2 Числа", () => {
     expect(sum()).toBe(16);
   });
   test("Задание 2, Почему 6.35.toFixed(1) == 6.3?", () => {
-    expect(+6.35.toFixed(1)).toBe(6.3);
+    expect(+(6.35).toFixed(1)).toBe(6.3);
     expect(Math.round(6.35 * 10) / 10).toBe(6.4);
   });
 
-  test('Задание 3, Ввод числового значения', ()=>{
-    
+  test("Задание 3, Ввод числового значения", () => {
     // jest.spyOn(window, "prompt").mockImplementation(() => null);
-    
-    
-    
-    function readNumber(input){
-      do {
-      } while(!isFinite(input))
-      if (input === null || input === '') return null;
 
-      return input
+    function readNumber(input) {
+      if (input === null || input === "") return null;
+      return input;
     }
-    jest.spyOn(window, "prompt").mockImplementation(() => '');
+    jest.spyOn(window, "prompt").mockImplementation(() => "");
     let input = prompt("Введите число");
-    expect(readNumber(input)).toBe(null)
+    expect(readNumber(input)).toBe(null);
     jest.spyOn(window, "prompt").mockImplementation(() => 14);
     input = prompt("Введите число");
-    expect(readNumber(input)).toBe(14)
-  })
+    expect(readNumber(input)).toBe(14);
+  });
+
+  test("Задание 4, Случайное число от min до max", () => {
+    const random = (a, b) => a + Math.random() * (b - a);
+    let randomResult = 1 + 0.123456789 * (4 - 1);
+    jest.spyOn(global.Math, "random").mockReturnValue(0.123456789);
+    expect(random(1, 4)).toBe(randomResult);
+
+    randomResult = 1 + 0.823093696478205 * (4 - 1);
+    jest.spyOn(global.Math, "random").mockReturnValue(0.823093696478205);
+    expect(random(1, 4)).toBe(randomResult);
+  });
+
+  test("Задание 5, Случайное целое число от min до max", () => {
+    const random = (a,b) => Math.round(a + Math.random() * (b-a) );
+    let randomResult = Math.round(1 + 0.123456789 * (5 - 1));
+    jest.spyOn(global.Math, "random").mockReturnValue(0.123456789);
+    expect(random(1, 5)).toBe(randomResult);
+
+    randomResult = Math.round(1 + 0.823093696478205 * (5 - 1));
+    jest.spyOn(global.Math, "random").mockReturnValue(0.823093696478205);
+    expect(random(1, 5)).toBe(randomResult);
+  });
+  
 });
