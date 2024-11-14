a4b6fb39547ecb96662015f02587fe9285f24050
Oganes <og.marckaryan@yandex.ru>
2024-11-14 20:22:36 +0300

    5.4 Массивы

---
+++ src/js/5.4 Массивы.test.js
@@ -0,0 +1,37 @@
+describe("5.3 Строки", () => {
+  test("Задание 1, Скопирован ли массив?", () => {
+    let fruits = ["Яблоки", "Груша", "Апельсин"];
+    expect(fruits.length).toBe(3);
+    let shoppingCart = fruits;
+    shoppingCart.push("Банан");
+
+    expect(fruits.length).toBe(4);
+  });
+
+  test("Задание 2, Операции с массивами", () => {
+    let styles = ["Джаз", "Блюз"];
+    styles.push("Рок-н-ролл");
+    expect(styles.length).toBe(3);
+    expect(styles).toEqual(["Джаз", "Блюз", "Рок-н-ролл"]);
+
+    styles[Math.floor(styles.length / 2)] = "Классика";
+    expect(styles).toEqual(["Джаз", "Классика", "Рок-н-ролл"]);
+
+    let word = styles.shift();
+    expect(word).toBe("Джаз");
+
+    styles.unshift("Рэп", "Регги");
+    expect(styles).toEqual(["Рэп", "Регги", "Классика", "Рок-н-ролл"]);
+  });
+
+  test("Задание 3, Вызов контекста", () => {
+    logSpy = jest.spyOn(global.console, "log");
+    let arr = ["a", "b"];
+
+    arr.push(function () {
+      console.log(this);
+    });
+    arr[2]();
+    expect(logSpy).toHaveBeenCalledWith(arr);
+  });
+});
