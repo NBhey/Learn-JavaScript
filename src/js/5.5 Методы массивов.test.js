9a851a6d8fda661b17e40607a3c3a31bcc7d076f
Oganes <og.marckaryan@yandex.ru>
2024-11-17 16:49:38 +0300

    5.5 Методы массивов, задание 2

--- src/js/5.5 Методы массивов.test.js
+++ src/js/5.5 Методы массивов.test.js
@@ -12,16 +12,22 @@ describe("5.5 Методы массивов", () => {
     expect(camelize("list-style-image")).toBe("listStyleImage");
     expect(camelize("-webkit-transition")).toBe("WebkitTransition");
 
-    function camelize2(str) {
-      return str
+    const camelize2 = (str) =>
+      str
         .split("-")
         .map((el, index) =>
           index === 0 ? el : el[0].toUpperCase() + el.slice(1)
         )
         .join("");
-    }
+
     expect(camelize2("background-color")).toBe("backgroundColor");
     expect(camelize2("list-style-image")).toBe("listStyleImage");
     expect(camelize2("-webkit-transition")).toBe("WebkitTransition");
+  });
+
+  test("Задание 2, фильтрация по диапазону", () => {
+    const filterRange = (arr, a, b) => arr.filter((el)=> el >= a && el <= b ? el : false);
+    expect(filterRange([5,3,8,1],1,4)).toEqual([3,1])
+    expect(filterRange([1,2,3,4,6,7,9],3,8)).toEqual([3,4,6,7])
   });
 });
