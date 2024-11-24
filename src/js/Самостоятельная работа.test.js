59d077c3303b7f6b967e80e471d6e3848b59971f
Oganes <og.marckaryan@yandex.ru>
2024-11-24 18:17:15 +0300

    Самостоятельная работа

--- src/js/Самостоятельная работа.test.js
+++ src/js/Самостоятельная работа.test.js
@@ -218,4 +218,96 @@ describe("Самостоятельная работа", () => {
       "Общее количество сообщений: 18 \nОтредактированных: 3\nНе отредактированных: 15"
     );
   });
+
+  test("Задача 4, Создайте функцию getUsersNamesInAgeRange(users, gender), которая возвращает среднее значение возраста пользователей одного пола.", () => {
+    const people = [
+      {
+        firstName: "Александр",
+        secondName: "Карпов",
+        age: 17,
+        gender: "мужской",
+      },
+      { firstName: "Егор", secondName: "Морозов", age: 21, gender: "мужской" },
+      {
+        firstName: "Мелисса",
+        secondName: "Леонова",
+        age: 40,
+        gender: "женский",
+      },
+      {
+        firstName: "Мелания",
+        secondName: "Савельева",
+        age: 37,
+        gender: "женский",
+      },
+      {
+        firstName: "Мария",
+        secondName: "Овчинникова",
+        age: 18,
+        gender: "женский",
+      },
+      {
+        firstName: "Марьяна",
+        secondName: "Котова",
+        age: 17,
+        gender: "женский",
+      },
+      {
+        firstName: "Фёдор",
+        secondName: "Селезнев",
+        age: 50,
+        gender: "мужской",
+      },
+      {
+        firstName: "Георгий",
+        secondName: "Петров",
+        age: 35,
+        gender: "мужской",
+      },
+      {
+        firstName: "Даниил",
+        secondName: "Андреев",
+        age: 49,
+        gender: "мужской",
+      },
+      {
+        firstName: "Дарья",
+        secondName: "Савельева",
+        age: 25,
+        gender: "женский",
+      },
+      { firstName: "Михаил", secondName: "Шаров", age: 22, gender: "мужской" },
+      {
+        firstName: "Владислав",
+        secondName: "Давыдов",
+        age: 40,
+        gender: "мужской",
+      },
+      { firstName: "Илья", secondName: "Казаков", age: 35, gender: "мужской" },
+      {
+        firstName: "Евгений",
+        secondName: "Кузьмин",
+        age: 19,
+        gender: "мужской",
+      },
+    ];
+
+    function getUsersNamesInAgeRange(users, gender) {
+      if (gender !== "женский" && gender !== "мужской") {
+        return 0;
+      }
+      if (users.length === 0) {
+        return 0;
+      }
+      let sum = 0;
+      let result = users
+        .filter((el) => el.gender === gender)
+        .map((el) => (sum = el.age + sum));
+      return sum / result.length;
+    }
+    expect(getUsersNamesInAgeRange(people, "мужской")).toBe(32)
+    expect(getUsersNamesInAgeRange(people, "женский")).toBe(27.4)
+    expect(getUsersNamesInAgeRange([], "женский")).toBe(0)
+    expect(getUsersNamesInAgeRange(people, "инопланетянин")).toBe(0)
+  });
 });
