307c293099c0ac5ac7a624a701316cde874066e3
Oganes <og.marckaryan@yandex.ru>
2024-12-02 18:33:48 +0300

    5.9 Object.keys, values, enteries

--- src/js/5.9 Object.keys, values,enteries.test.js
+++ src/js/5.9 Object.keys, values,enteries.test.js
@@ -1,21 +1,38 @@
 describe("5.9 Map и Set", () => {
-    test('Задание 1, сумма свойств объекта', ()=>{
-        let salaries = {
-            "John": 100,
-            "Pete": 300,
-            "Mary": 250
-          };
+  test("Задание 1, сумма свойств объекта", () => {
+    let salaries = {
+      John: 100,
+      Pete: 300,
+      Mary: 250,
+    };
 
-          function sumSalaries(salaries) {
+    function sumSalaries(obj) {
+      let sum = 0;
+      for (let el of Object.values(obj)) {
+        sum += el;
+      }
+      return sum;
+    }
 
-            let sum = 0;
-            for (let salary of Object.values(salaries)) {
-              sum += salary;
-            }
-          
-            return sum; 
-          }
-          expect(sumSalaries(salaries)).toBe(650)
-    })
+    expect(sumSalaries(salaries)).toBe(650);
   });
-  
+
+  test("Задание 2, подсчет количества свойств объекта", () => {
+    let user = {
+      name: "John",
+      age: 30,
+    };
+
+    let count = (obj) => Object.keys(obj).length;
+
+    expect(count(user)).toBe(2);
+
+    let user2 = {
+      name: "John",
+      age: 30,
+      surname: "Johnson",
+      hobby: "football",
+    };
+    expect(count(user2)).toBe(4);
+  });
+});
