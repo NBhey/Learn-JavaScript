fac1be63cc967851ef505b4c8d608a4cc06710a6
Oganes <og.marckaryan@yandex.ru>
2024-12-07 19:34:55 +0300

    test2

--- src/js/2.10 Условное ветвление.js
+++ src/js/2.10 Условное ветвление.js
@@ -1,26 +1,26 @@
-export function checkToBeTruthy(){
-    if ("0") {
-        alert( 'Привет' );
-      }
+export function checkToBeTruthy() {
+  if ("0") {
+    alert("Привет");
+  }
 }
 
-export function whatOfficialNameJS(){
-    let input = prompt('Какое «официальное» название JavaScript?')
+export function whatOfficialNameJS() {
+  let input = prompt("Какое «официальное» название JavaScript?");
 
-    if(input === "ECMAScript"){
-        alert('Верно!')
-    }else{
-        alert('Не знаете? ECMAScript!')
-    }
+  if (input === "ECMAScript") {
+    alert("Верно!");
+  } else {
+    alert("Не знаете? ECMAScript!");
+  }
 }
 
-export function getNumber(){
-    let input = +prompt('Введите любое число')
-    if (input > 0) {
-        alert(1)
-    } else if(input < 0) {
-        alert(-1)
-    } else {
-        alert(0)
-    }
+export function getNumber() {
+  let input = +prompt("Введите любое число");
+  if (input > 0) {
+    alert(1);
+  } else if (input < 0) {
+    alert(-1);
+  } else {
+    alert(0);
+  }
 }
