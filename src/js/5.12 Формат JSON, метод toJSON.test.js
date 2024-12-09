3a3c725d423273c01e10e5ad781c262c2e606953
Oganes <og.marckaryan@yandex.ru>
2024-12-09 20:42:48 +0300

    5.12 Формат JSON, метод toJSON, задание 2

--- src/js/5.12 Формат JSON, метод toJSON.test.js
+++ src/js/5.12 Формат JSON, метод toJSON.test.js
@@ -11,4 +11,27 @@ describe("5.12 Формат JSON, метод toJSON", () => {
       age: 35,
     });
   });
+  test("Задание 2,Исключить обратные ссылки", () => {
+    let room = {
+      number: 23,
+    };
+
+    let meetup = {
+      title: "Совещание",
+      occupiedBy: [{ name: "Иванов" }, { name: "Петров" }],
+      place: room,
+    };
+    room.occupiedBy = meetup;
+    meetup.self = meetup;
+
+    let result = JSON.parse(JSON.stringify(meetup, function replacer(key, value) {
+        return (key != "" && value == meetup) ? undefined : value;
+    }));
+
+    expect(result).toEqual({
+      title: "Совещание",
+      occupiedBy: [{ name: "Иванов" }, { name: "Петров" }],
+      place: { number: 23 },
+    });
+  });
 });
