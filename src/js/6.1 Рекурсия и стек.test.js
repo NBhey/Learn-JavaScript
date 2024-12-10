355d4361c33bdc483133cc4f92a5b31fadd7fb34
Oganes <og.marckaryan@yandex.ru>
2024-12-10 20:18:55 +0300

    6.1 Рекурсия и стек, Задание 1, пункт 2

--- src/js/6.1 Рекурсия и стек.test.js
+++ src/js/6.1 Рекурсия и стек.test.js
@@ -12,5 +12,18 @@ describe("6.1 Рекурсия и стек", () => {
     expect(sumTo(3)).toBe(6);
     expect(sumTo(4)).toBe(10);
     expect(sumTo(100)).toBe(5050);
+
+    function sumTo2(n) {
+      let result = 0;
+      for (let i = 0; i <= n; i += 1) {
+        result += i;
+      }
+      return result;
+    }
+    expect(sumTo2(1)).toBe(1);
+    expect(sumTo2(2)).toBe(3);
+    expect(sumTo2(3)).toBe(6);
+    expect(sumTo2(4)).toBe(10);
+    expect(sumTo2(100)).toBe(5050);
   });
 });
