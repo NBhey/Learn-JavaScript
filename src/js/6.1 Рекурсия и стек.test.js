746070c476174cc3f2bd6751f13d33285276139f
Oganes <og.marckaryan@yandex.ru>
2024-12-12 21:03:12 +0300

    6.1 Рекурсия и стек, задание 4, пункт 1

--- src/js/6.1 Рекурсия и стек.test.js
+++ src/js/6.1 Рекурсия и стек.test.js
@@ -63,15 +63,49 @@ describe("6.1 Рекурсия и стек", () => {
     function fib(n) {
       let fibArr = [0, 1];
       for (let i = 1; i <= n; i += 1) {
-          fibArr.push(fibArr[i] + fibArr[i - 1]);
+        fibArr.push(fibArr[i] + fibArr[i - 1]);
       }
 
-      return fibArr[n]
+      return fibArr[n];
     }
-    expect(fib(2)).toBe(1)
-    expect(fib(3)).toBe(2)
-    expect(fib(7)).toBe(13)
-    expect(fib(77)).toBe(5527939700884757)
-    expect(fib(6)).toBe(8)
+    expect(fib(2)).toBe(1);
+    expect(fib(3)).toBe(2);
+    expect(fib(7)).toBe(13);
+    expect(fib(77)).toBe(5527939700884757);
+    expect(fib(6)).toBe(8);
+  });
+
+  test("Задание 4, Вывод односвязного списка", () => {
+    const logSpy = jest.spyOn(global.console, "log");
+
+    let list = {
+      value: 1,
+      next: {
+        value: 2,
+        next: {
+          value: 3,
+          next: {
+            value: 4,
+            next: null,
+          },
+        },
+      },
+    };
+
+    function printList(list) {
+      let objectValue = list;
+
+      while (objectValue) {
+        console.log(objectValue.value);
+        objectValue = objectValue.next;
+      }
+    }
+    printList(list)
+
+    expect(logSpy).toHaveBeenCalledWith(1)
+    expect(logSpy).toHaveBeenCalledWith(2)
+    expect(logSpy).toHaveBeenCalledWith(3)
+    expect(logSpy).toHaveBeenCalledWith(4)
+
   });
 });
