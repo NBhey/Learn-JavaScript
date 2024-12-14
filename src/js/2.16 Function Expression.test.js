3cfa6c67bcd933175de6fd73ddd7832557458e5a
Oganes <og.marckaryan@yandex.ru>
2024-12-14 19:23:16 +0300

    6.3 Область видимости переменных, замыкание, задание 2

--- src/js/2.16 Function Expression.test.js
+++ src/js/2.16 Function Expression.test.js
@@ -1,14 +1,13 @@
 describe("2.16 Function Expression", () => {
-    test('2.16 Function Expression', ()=>{
-      let sum = function(a,b){
-        return a + b
-      };
-      expect(sum(3,3)).toBe(6)
-      expect(sum).toBeInstanceOf(Function)
+  test("2.16 Function Expression", () => {
+    let sum = function (a, b) {
+      return a + b;
+    };
+    expect(sum(3, 3)).toBe(6);
+    expect(sum).toBeInstanceOf(Function);
 
-      let func = sum;
-      expect(func(4,3)).toBe(7)
-      expect(func).toBeInstanceOf(Function)
-    })
+    let func = sum;
+    expect(func(4, 3)).toBe(7);
+    expect(func).toBeInstanceOf(Function);
   });
-  
+});
--- src/js/6.3 Область видимости переменных, замыкание.test.js
+++ src/js/6.3 Область видимости переменных, замыкание.test.js
@@ -1,15 +1,28 @@
 describe("6.3 Область видимости переменных, замыкание", () => {
+  const logSpy = jest.spyOn(global.console, "log");
   test("Учитывает ли функция последние изменения?", () => {
     let name = "John";
-    const logSpy = jest.spyOn(global.console, "log");
     function sayHi() {
       console.log("Hi, " + name);
     }
-
     name = "Pete";
-
     sayHi(); // что будет показано: "John" или "Pete"?
-
     expect(logSpy).toHaveBeenCalledWith("Hi, Pete");
+  });
+
+  test("Задание 2, Какие переменные доступны", () => {
+    function makeWorker() {
+      let name = "Pete";
+
+      return function () {
+        console.log(name);
+      };
+    }
+    let name = "John";
+    // создаём функцию
+    let work = makeWorker();
+    // вызываем её
+    work(); // что будет показано?
+    expect(logSpy).toHaveBeenCalledWith("Pete");
   });
 });
