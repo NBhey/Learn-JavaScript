96a10dedda99233e4ff0b4662498f9efa571f528
Oganes <og.marckaryan@yandex.ru>
2024-12-27 21:27:34 +0300

    6.2 Остаточные параметры и оператор расширения, самостоятельная работа 3

--- src/js/6.2 Остаточные параметры и оператор расширения.test.js
+++ src/js/6.2 Остаточные параметры и оператор расширения.test.js
@@ -7,13 +7,28 @@ describe("6.2 Остаточные параметры и оператор рас
   });
 
   test("Самостоятельная работа, задание 2", () => {
-    function sumAll(...args) { // args — имя массива
-        let sum = 0;
-      
-        for (let arg of args) sum += arg;
-      
-        return sum;
-      }
+    function sumAll(...args) {
+      // args — имя массива
+      let sum = 0;
+
+      for (let arg of args) sum += arg;
+
+      return sum;
+    }
     expect(sumAll(1, 2, 3, 4, 5)).toBe(15);
+  });
+
+  test("Самостоятельная работа, задание 3", () => {
+    const logSpy = jest.spyOn(window.console, "log");
+    function showName() {
+      // args — имя массива
+      console.log(arguments.length);
+      console.log(arguments[2]);
+      console.log(arguments[1]);
+    }
+    showName("один", 2, "камень");
+    expect(logSpy).toHaveBeenCalledWith("камень");
+    expect(logSpy).toHaveBeenCalledWith(3);
+    expect(logSpy).toHaveBeenCalledWith(2);
   });
 });
