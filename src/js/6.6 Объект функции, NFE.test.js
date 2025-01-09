b715753855a8cde6946efb291c1321c22614d982
Oganes <og.marckaryan@yandex.ru>
2025-01-09 20:50:17 +0300

    6.6 Объект функции, NFE, задание 1

--- src/js/6.6 Объект функции, NFE.test.js
+++ src/js/6.6 Объект функции, NFE.test.js
@@ -1,28 +1,42 @@
 describe("6.6 Объект функции, NFE.test", () => {
-  test("Задание 2, Сумма с произвольным количеством скобок", () => {
-    window.alert = jest.fn()
-    function sum(a) {
+  test("Задание 1, Установка и уменьшение значения счётчика", () => {
+    function makeCounter() {
+      let count = 0;
 
-        let currentSum = a;
-      
-        function f(b) {
-            if(b !== undefined){
-                currentSum += b;
-                return currentSum;
-            }
-          
-          
-        }
-      
-        f.toString = function() {
-          return currentSum;
-        };
-      
-        return f;
+      function counter() {
+        return count++;
       }
-      
-    alert( sum(1)(2) )
+      counter.set = (value) => (count = value);
+      counter.decrease = () => count--;
+      return counter;
+    }
+    let test = makeCounter();
+    expect(test()).toBe(0);
+    expect(test()).toBe(1);
+    expect(test()).toBe(2);
+  });
+
+  test("Задание 2, Сумма с произвольным количеством скобок", () => {
+    window.alert = jest.fn();
+    function sum(a) {
+      let currentSum = a;
+
+      function f(b) {
+        if (b !== undefined) {
+          currentSum += b;
+          return currentSum;
+        }
+      }
+
+      f.toString = function () {
+        return currentSum;
+      };
+
+      return f;
+    }
+    let testF = sum(1)(2);
+    expect(testF).toBe(3);
+    alert(sum(1)(2));
     expect(window.alert).toHaveBeenCalledWith(3);
-   
   });
 });
