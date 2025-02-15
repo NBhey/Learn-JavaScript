961bfc688dc2cc3dc1dbdfc0888220b4f707279b
Oganes <og.marckaryan@yandex.ru>
2025-02-15 19:10:12 +0300

    Самостоятелньая работа Aston

--- src/js/Самостоятельная работа Aston.test.js
+++ src/js/Самостоятельная работа Aston.test.js
@@ -1,15 +1,18 @@
-var globalVar = "global";
-var outerVar = "outer";
+describe("Самостоятельная работа Aston", () => {
+  test("Задание", () => {
+    var globalVar = "global";
+    var outerVar = "outer";
 
-function outerFunc(outerParam) {
-  function innerFunc(innerParam) {
-    console.log(globalVar, outerParam, innerParam); // guess,outer,inner
-  }
-  return innerFunc;
-}
+    function outerFunc(outerParam) {
+      function innerFunc(innerParam) {
+        return [globalVar, outerParam, innerParam]; // guess,outer,inner
+      }
+      return innerFunc;
+    }
 
-const x = outerFunc(outerVar);
-outerVar = "outer-2";
-globalVar = "guess";
-x("inner");//guess, outer-2, inner
-// разобрался почему outer будет а не outer-2, ответ оставил неправильным для себя
+    const x = outerFunc(outerVar);
+    outerVar = "outer-2";
+    globalVar = "guess";
+    expect(x("inner")).toEqual(['guess',"outer","inner"]);
+  });
+});
