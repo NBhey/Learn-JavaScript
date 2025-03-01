5a8e1e7f2031baee994511c3416f5098f723b7b6
Oganes <og.marckaryan@yandex.ru>
2025-03-01 17:42:56 +0300

    подготовка к собеседованию

--- src/js/Самостоятельная работа Aston.test.js
+++ src/js/Самостоятельная работа Aston.test.js
@@ -13,6 +13,48 @@ describe("Самостоятельная работа Aston", () => {
     const x = outerFunc(outerVar);
     outerVar = "outer-2";
     globalVar = "guess";
-    expect(x("inner")).toEqual(['guess',"outer","inner"]);
+    expect(x("inner")).toEqual(["guess", "outer", "inner"]);
+  });
+
+  test("", () => {
+    const testObj = {
+      method: () => {
+        return `Hi ${this}`;
+      },
+    };
+    const testObj2 = {
+      name: "John",
+    };
+
+    testObj2.__proto__ = testObj;
+
+    expect(testObj2.method()).toBe("Hi undefined");
+  });
+
+  test("", () => {
+    function test() {
+      return `Меня зовут ${this.name}`;
+    }
+
+    const user = {
+      name: "John",
+    };
+    expect(test.call(user)).toBe(`Меня зовут John`);
+  });
+
+  test("Область видимости", () => {
+    let logSpy = jest.spyOn(global.console, "log");
+    let a = 10;
+    function func() {
+      console.log(a);
+    }
+    const wrap = () => {
+      let a = 30;
+      func();
+    };
+    a = 20;
+
+    wrap();
+    expect(logSpy).toHaveBeenCalledWith(20);
   });
 });
