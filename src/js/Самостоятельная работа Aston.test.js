5886f24816d04c6bf3c2f22d23acfa6b74563fe9
Oganes <og.marckaryan@yandex.ru>
2025-03-02 19:04:25 +0300

    неявное преобразование типов. Самостоятельная работа Aston

--- src/js/Самостоятельная работа Aston.test.js
+++ src/js/Самостоятельная работа Aston.test.js
@@ -1,4 +1,6 @@
 describe("Самостоятельная работа Aston", () => {
+  let logSpy = jest.spyOn(global.console, "log");
+
   test("Задание", () => {
     var globalVar = "global";
     var outerVar = "outer";
@@ -74,9 +76,8 @@ describe("Самостоятельная работа Aston", () => {
   });
 
   test("___", () => {
-    let logSpy = jest.spyOn(global.console, "log");
     function Hamster() {
-      this.food = []
+      this.food = [];
     }
 
     Hamster.prototype.found = function (something) {
@@ -96,5 +97,73 @@ describe("Самостоятельная работа Aston", () => {
     expect(logSpy).toHaveBeenCalledWith(0);
   });
 
+  test("class", () => {
+    class Animal {
+      constructor(name) {
+        this.name = name;
+      }
+    }
 
+    class Rabbit extends Animal {
+      constructor(name) {
+        super(name);
+        this.name = name;
+        this.created = Date.now();
+      }
+    }
+
+    let rabbit = new Rabbit("Белый кролик"); // Error: this is not defined
+    console.log(rabbit.name);
+    expect(logSpy).toHaveBeenCalledWith("Белый кролик");
+  });
+  test("Что выведет консоль", () => {
+    expect("B" + "a" + (1 - "hello")).toBe("BaNaN");
+    expect((true && 3) + "d").toBe("3d");
+    expect(Boolean(true && 3) + "d").toBe("trued");
+    expect(NaN + 1).toBe(NaN);
+    expect(NaN + "o").toBe("NaNo");
+    expect(undefined + 1).toBe(NaN);
+    expect(undefined - 1).toBe(NaN);
+    expect(null + 1).toBe(1);
+    expect(null / 5).toBe(0);
+    expect(null == 0).toBe(false);
+    expect(null == "").toBe(false);
+    expect(null > 0).toBe(false);
+    expect(null == "").toBe(false);
+    expect("foo" + +"bar").toBe("fooNaN");
+    expect("11" + "1" - 1).toBe(110);
+
+    expect(true + false).toBe(1);
+    expect(12 / "6").toBe(2);
+    expect(12 / "+").toBe(NaN);
+    expect("number" + 15 + 3).toBe("number153");
+    expect([1] > null).toBe(true);
+    expect("true" == true).toBe(false);
+    expect("true" === true).toBe(false);
+    expect("false" == false).toBe(false);
+    expect(null == "").toBe(false);
+    expect(!!"false" == !!"true").toBe(true);
+    expect(["x"] == "x").toBe(true);
+    expect([] + null + 1).toBe("null1");
+    expect(0 || ("0" && {})).toBe(true);
+    expect([1, 2, 3] == [1, 2, 3]).toBe(false);
+  });
 });
+// {Array.prototype.myMap = function(callback, thisArg) {
+//   console.log('thisArg 1',thisArg )
+//   const result = [];
+//   for (let i = 0; i < this.length; i++) {
+//       result.push(callback.call(thisArg, this[i], i, this));
+//       console.log('myMap start')
+//       console.log('thisArg 2',thisArg )
+//       console.log('this[i]', this[i])
+//       console.log('i', i)
+//       console.log('this', this)
+//       console.log('myMap finish')
+//   }
+//   return result;
+// };
+
+// const arr = [1, 2, 3, 4, 5];
+// const doubled = arr.myMap((num) => num * 2);
+// console.log(doubled); }
